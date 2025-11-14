9d6fd47f31b19d36613c8041fa1470f9037477c1
Nate <nmoore1@mines.edu>
2025-11-14 16:56:56 -0700

    Bounds + Sanity checking; created a mapping table for the resetter

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -20,7 +21,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/reset", serveFile)
-	//http.HandleFunc("/test.html", serveFile)
 	http.HandleFunc("/reset/", resetHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -29,28 +29,98 @@ func main() {
 func resetHandler(writer http.ResponseWriter, request *http.Request) {
 	split := strings.Split(request.URL.String(), "/")
 
-	err := exec.Command("qm", "stop", split[3]).Run()
-	if err != nil {
-		log.Println("qm stop " + split[3] + "failed")
+	type vmFormat struct {
+		templateID int
+		vmID       [10]int
+		name       string
 	}
 
-	err = exec.Command("qm", "destroy", split[3]).Run()
-	if err != nil {
-		log.Println("qm destroy " + split[3] + "failed")
+	type vmid int
+
+	// Templates
+	var (
+		UbuntuEasy1   = 110
+		UbuntuEasy2   = 111
+		UbuntuMedium1 = 120
+		UbuntuMedium2 = 121
+		SupportEasy1  = 210
+		SupportEasy2  = 211
+	)
+
+	// mappings
+	var VMMap = map[int]vmFormat{
+		UbuntuEasy1:   {templateID: 110, vmID: [10]int{1101}, name: "Ubuntu-Easy-1"},
+		UbuntuEasy2:   {templateID: 111, vmID: [10]int{1111}, name: "Ubuntu-Easy-2"},
+		UbuntuMedium1: {templateID: 120, vmID: [10]int{1201}, name: "Ubuntu-Medium-1"},
+		UbuntuMedium2: {templateID: 121, vmID: [10]int{1211}, name: "Ubuntu-Medium-2"},
+		SupportEasy1:  {templateID: 210, vmID: [10]int{2101}, name: "Support-Easy-1"},
+		SupportEasy2:  {templateID: 211, vmID: [10]int{2111}, name: "Support-Easy-2"},
 	}
 
-	err = exec.Command("qm", "clone", split[2], split[3], "--name", split[4]).Run()
+	var skip = false
+
+	targetName := split[4]
+
+	templateID, err := strconv.Atoi(split[2])
 	if err != nil {
-		log.Println("qm clone " + split[2] + "->" + split[3] + "failed")
+		skip = true
+		log.Println("Failed to convert")
+	}
+
+	targetID, err := strconv.Atoi(split[3])
+	if err != nil {
+		skip = true
+		log.Println("Failed to convert")
+	}
+
+	var corr = false
+
+	if (targetID / 10) != templateID {
+		skip = true
+	}
+
+	if !skip {
+		for _, vm := range VMMap {
+			if vm.templateID == templateID {
+				for _, vmID := range vm.vmID {
+					if vmID == targetID && vm.name == targetName {
+						corr = true
+					}
+				}
+			}
+		}
+	}
+
+	if corr == false || skip {
+		errorMSG := "Request Parameter Match Error. Check for valid VMID, TemplateID, or Name"
+		writer.WriteHeader(400)
+		writer.Write([]byte(errorMSG))
+		log.Fatal(errorMSG)
+		return
+	}
+
+	err = exec.Command("qm", "stop", strconv.Itoa(targetID)).Run()
+	if err != nil {
+		log.Println("qm stop " + strconv.Itoa(targetID) + "failed")
+	}
+
+	err = exec.Command("qm", "destroy", strconv.Itoa(targetID)).Run()
+	if err != nil {
+		log.Println("qm destroy " + strconv.Itoa(targetID) + "failed")
+	}
+
+	err = exec.Command("qm", "clone", strconv.Itoa(templateID), strconv.Itoa(targetID), "--name", targetName).Run()
+	if err != nil {
+		log.Println("qm clone " + strconv.Itoa(templateID) + "->" + strconv.Itoa(targetID) + "failed")
 	}
 
 	// pvesh set /access/acl -path /vms/{vmid} -roles PVEVMUser -groups SD_Users
-	cmd := exec.Command("pvesh", "set", "/access/acl", "-path", "/vms/"+split[3], "-roles", "PVEVMUser", "-groups", "SD_Users")
+	cmd := exec.Command("pvesh", "set", "/access/acl", "-path", "/vms/"+strconv.Itoa(targetID), "-roles", "PVEVMUser", "-groups", "SD_Users")
 
 	err = cmd.Run()
 	if err != nil {
-		log.Println("pvesh " + "set " + "/access/acl " + "-path " + "/vms/" + split[3] + " -roles " + "PVEVMUser " + "-groups " + "SD_Users " + "failed")
+		log.Println("pvesh " + "set " + "/access/acl " + "-path " + "/vms/" + strconv.Itoa(targetID) + " -roles " + "PVEVMUser " + "-groups " + "SD_Users " + "failed")
 	}
 
-	log.Println(split[2], split[3], split[4])
+	log.Println("Template: "+strconv.Itoa(templateID), "Target: "+strconv.Itoa(targetID), targetName)
 }
